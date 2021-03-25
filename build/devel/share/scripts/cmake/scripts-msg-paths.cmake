27fc2b778f2a520ae3da5bd093cdbcc31cf370bc
janfrischmuth <jan.frischmuth@me.com>
2021-03-25 11:11:30 +0100

    all missing build files etc

---
+++ build/devel/share/scripts/cmake/scripts-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(scripts_MSG_INCLUDE_DIRS "/home/jan/turtlebot_ws/src/scripts/msg")
+set(scripts_MSG_DEPENDENCIES std_msgs;geometry_msgs)
