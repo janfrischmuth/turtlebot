d229adc573df67b28d7512a05c027919f48d79d2
janfrischmuth <jan.frischmuth@me.com>
2021-04-21 17:49:18 +0200

    new branch for qualisys implementation

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,7 +8,7 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/jan/turtlebot_ws/src/scripts/build/devel:$CMAKE_PREFIX_PATH"
-export PATH='/home/jan/turtlebot_ws/devel/bin:/opt/ros/melodic/bin:/home/jan/.local/bin:/home/jan/bin:/home/jan/.local/bin:/home/jan/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PATH='/home/jan/turtlebot_ws/devel/bin:/opt/ros/melodic/bin:/home/jan/.local/bin:/home/jan/bin:/home/jan/miniconda2/bin:/home/jan/miniconda2/condabin:/home/jan/.local/bin:/home/jan/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 export PWD='/home/jan/turtlebot_ws/src/scripts/build'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/jan/turtlebot_ws/src/scripts/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/jan/turtlebot_ws/src/scripts:$ROS_PACKAGE_PATH"
