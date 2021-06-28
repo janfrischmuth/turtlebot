25e2e3c6297f17db68351f6b331a69b05197f27b
janfrischmuth <jan.frischmuth@me.com>
2021-06-28 21:23:02 +0200

    now drives to all coordinates but some angular speeds are weird

--- src/OdometryController.py
+++ src/OdometryController.py
@@ -2,7 +2,7 @@
 
 # controller for the odometry. sets robot in motion.
 
-from math import atan2, sqrt  # , pi
+from math import atan2, sqrt, pi, fmod
 import rospy
 from sys import exit
 # from nav_msgs.msg import Odometry
@@ -11,6 +11,7 @@ from geometry_msgs.msg import Point, Twist, Pose  # , Quaternion
 from nav_msgs.msg import Odometry
 # from std_msgs.msg import String
 from scripts.srv import *
+import angles
 
 
 class Controller:
@@ -39,10 +40,10 @@ class Controller:
 
         # params for turtlebot
         self.v_max_lin = 0.3  # max. linear velocity of turtlebot2/kobuki
-        self.v_max_ang = 1  # 3.14  # in [rad/s], max. rotational velocity
-        self.accuracy_dist = 0.1
-        self.accuracy_ang = 0.17  # equals 10deg
-        self.K_vel_lin = 1  # proportional gain
+        self.v_max_ang = 1.0  # 3.14  # in [rad/s], max. rotational velocity
+        self.accuracy_dist = 0.1  # [m]
+        self.accuracy_ang = 0.17  # [rad]. 0.17 rad = 10 deg
+        self.K_vel_lin = 1.0  # proportional gain
         self.K_vel_ang = 1.0  # proportional gain
         self.rate = rospy.Rate(10)  # Hz
 
@@ -89,7 +90,7 @@ class Controller:
 
         x_diff = self.goal.x - self.CurrPos.x
         y_diff = self.goal.y - self.CurrPos.y
-        self.PosDiff = sqrt((x_diff)**2 + (y_diff)**2)
+        self.PosDiff = sqrt((x_diff)**2.0 + (y_diff)**2.0)
 
         CurrOrient = CurrPosVar.pose.pose.orientation
         CurrOrientQuaternion = [CurrOrient.x,
@@ -97,11 +98,14 @@ class Controller:
 
         (self.roll, self.pitch, self.yaw) = euler_from_quaternion(
             CurrOrientQuaternion)
+        self.yaw = angles.normalize_angle_positive(self.yaw)
 
         # alpha is the angle to goal in [Bogenmass]
-        self.alpha = (atan2(y_diff, x_diff))  # % (2*pi)
+        self.alpha = angles.normalize_angle_positive(
+            atan2(y_diff, x_diff))  # % (2*pi)
 
         self.angle_diff = (self.alpha - self.yaw)
+        #self.angle_diff = angles.shortest_angular_distance(self.alpha, self.yaw)
 
         v_lin = self.PosDiff * self.K_vel_lin
         v_ang = self.angle_diff * self.K_vel_ang
@@ -111,24 +115,57 @@ class Controller:
 
         # sets angular speed to max value if the calculated one is too big
         # considers signed value (+ or -)
-        if v_ang > 0.0:
+        if v_ang < pi:
             if v_ang < self.v_max_ang:
                 self.speed.angular.z = v_ang
+                print("------\nif if\nv_ang = %s" % v_ang)
+                print("self.speed.angular.z = %s\n------" %
+                      self.speed.angular.z)
             else:
                 self.speed.angular.z = self.v_max_ang
-        elif v_ang < 0.0:
-            if v_ang > -(self.v_max_ang):
-                self.speed.angular.z = v_ang
+                print("------\nif else\nv_ang = %s" % v_ang)
+                print("self.speed.angular.z = %s\n------" %
+                      self.speed.angular.z)
+        elif v_ang > pi:
+            v_ang -= pi
+            if v_ang < self.v_max_ang:
+                self.speed.angular.z = -v_ang
+                print("------\nelif if\nv_ang = %s" % v_ang)
+                print("self.speed.angular.z = %s\n------" %
+                      self.speed.angular.z)
             else:
-                self.speed.angular.z = -(self.v_max_ang)
+                self.speed.angular.z = -self.v_max_ang
+                print("------\nelif else\nv_ang = %s" % v_ang)
+                print("self.speed.angular.z = %s\n------" %
+                      self.speed.angular.z)
         elif v_ang == 0.0:
             self.speed.angular.z = v_ang
+            print("SURPRISE!!! v_ang = 0!!! NEVER HAPPENED BEFORE!!!")
 
         self.pubVel.publish(self.speed)
 
         # infos just for debugging
-        rospy.logdebug("\ngoal = (% s , % s )\nx= % s\ny= % s\nPosDiff= % s\nyaw= % s\nalpha= % s\nangle_diff= % s\nv_lin= % s\nv_ang= % s\n------\n------" %
-                       (self.goal.x, self.goal.y, self.CurrPos.x, self.CurrPos.y, self.PosDiff, self.yaw, self.alpha, self.angle_diff, self.speed.linear.x, self.speed.angular.z))
+        # DON'T log each var into own function call. is too heavy performance-wise
+        rospy.logdebug("\n goal #%s = ( % s , % s )\
+                        \n x = % s [m]\
+                        \n y = % s [m]\
+                        \n PosDiff = % s [m]\
+                        \n yaw = % s [deg]\
+                        \n alpha = % s [deg]\
+                        \n angle_diff = % s [deg]\
+                        \n v_lin = % s [m/s]\
+                        \n v_ang = % s [rad/s]\
+                        \n------\
+                        \n------"
+                       % (self.coord_count, self.goal.x, self.goal.y,
+                          self.CurrPos.x,
+                          self.CurrPos.y,
+                          self.PosDiff,
+                          (self.yaw * 180.0/pi),
+                          (self.alpha * 180.0/pi),
+                          (self.angle_diff*180.0/pi),
+                          self.speed.linear.x,
+                          self.speed.angular.z))
         # these might be interesting, too
         # rospy.logdebug("x_diff = %s" % x_diff)
         # rospy.logdebug("y_diff = %s" % y_diff)
@@ -146,7 +183,7 @@ class Controller:
 
 def main():
     rospy.sleep(3)
-    rospy.init_node('OdometryController', log_level=rospy.DEBUG)
+    rospy.init_node('OdometryController', log_level=rospy.INFO)
     print("node OdometryController successfully initialised")
     # function to set first coordinate? to prevent driving to (0,0) in the beginning!
     Controller()
